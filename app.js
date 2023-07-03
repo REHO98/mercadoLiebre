c4b8d476acec850fe8104c28e49af8a66ece46f7
Rodrigo E. Hidalgo <rodrihidalgo20@gmail.com>
2023-07-03 18:51:14 -0300

    agregado de estilos

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/home.html');
-});
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html")));
 
 app.use((req, res, next) => {
   res.status(404).send('Página no encontrada');
