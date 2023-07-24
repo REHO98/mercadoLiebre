130c6f96586769bb2a632e1d8f79c9c734ddebc0
Rodrigo E. Hidalgo <rodrihidalgo20@gmail.com>
2023-07-24 20:03:02 -0300

    agregado de css y arreglo de html

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ TP Mercado Liebre | Curso Fullstack Web | Fundacion Formar - Digital House
 [Mercado Liebre Parte 3](https://github.com/REHO98/mercadoLiebre/tree/mercado_liebre_part3)
 
 [Mercado Liebre Parte 4](https://github.com/REHO98/mercadoLiebre/tree/mercado_liebre_part4)
+
+[Mercado Liebre Parte 5](https://github.com/REHO98/mercadoLiebre/tree/mercado_liebre_part5)
--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+/* rutas */
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "views", "home.html")));
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
 
 app.use((req, res, next) => {
-  res.status(404).send('Página no encontrada');
+  res.status(404).send("Página no encontrada");
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Error del servidor');
+  res.status(500).send("Error del servidor");
 });
 
 app.listen(port, () => {
